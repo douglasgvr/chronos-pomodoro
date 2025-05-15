74ee6539e42ff8207ec224c7ef9b949e07b28591
douglasgvr <douglasfarmaangra@gmail.com>
2025-05-15 10:40:48 -0300

    criando o component Container e seu css

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,17 @@ import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
 import { TimerIcon } from 'lucide-react';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola Mundo
-        <button>
+      <Container>
+        <div>
           <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit porro
-        accusamus dolor. Temporibus maiores sunt quae labore neque! Illo
-        dignissimos modi reprehenderit a hic facilis est dolor obcaecati numquam
-        odio.
-      </p>
+          <Heading>Chronos</Heading>
+        </div>
+      </Container>
     </>
   );
 }

9db7154f63c3075e17bf83c1b50c6558014cce29
douglasgvr <douglasfarmaangra@gmail.com>
2025-05-23 09:16:22 -0300

    criando a estrutura do formulario da home

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,24 @@ export function App() {
       <Container>
         <CountDown />
       </Container>
+      <Container>
+        <form className='form'>
+          <div className='formRow'>
+            <label htmlFor='input'>task</label>
+            <input id='input' type='text' />
+          </div>
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+          <div className='formRow'>
+            <button>enviar</button>
+          </div>
+        </form>
+      </Container>
     </>
   );
 }
